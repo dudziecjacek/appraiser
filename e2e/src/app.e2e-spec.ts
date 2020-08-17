e64212dd49da4ecab93519d20a80af6108c0def7
jdudziec <dudziecjacek@gmail.com>
2020-08-17 14:55:01 +0200

    changed project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('lufz-ng app is running!');
+    expect(page.getTitleText()).toEqual('appraiser app is running!');
   });
 
   afterEach(async () => {
