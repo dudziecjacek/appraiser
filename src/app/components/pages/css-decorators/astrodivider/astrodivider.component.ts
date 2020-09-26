f3e8a3c7b2960985bac0ebb09914d7d917373fd6
jdudziec <dudziecjacek@gmail.com>
2020-09-26 18:39:22 +0200

    added about me section

--- src/app/components/pages/css-decorators/astrodivider/astrodivider.component.ts
+++ src/app/components/pages/css-decorators/astrodivider/astrodivider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-astrodivider',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./astrodivider.component.scss']
 })
 export class AstrodividerComponent implements OnInit {
+  @Input() isInverted: boolean;
 
   constructor() { }
 
